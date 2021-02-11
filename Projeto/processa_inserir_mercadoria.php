d8c7e81cd1ab650f47d3d6ee48f1b717b78190ff
LexiconFive <41704578+MacrosWizard@users.noreply.github.com>
2021-02-11 14:52:46 -0300

    tirando echos

--- Projeto/processa_inserir_mercadoria.php
+++ Projeto/processa_inserir_mercadoria.php
@@ -6,10 +6,6 @@ $produto = $_POST['produto'];
 $preco = $_POST['preco'];
 $quantidade = $_POST['quantidade'];
 
-echo $produto;
-echo $preco;
-echo $quantidade;
-
 $query = "INSERT INTO tabela (produto, preco, quantidade)
             VALUES('$produto', $preco, $quantidade)";
 
